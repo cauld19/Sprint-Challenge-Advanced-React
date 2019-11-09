ab3374cf7e96801c6bd8997a67834547df372e6f
cauld19 <50849710+cauld19@users.noreply.github.com>
2019-11-09 12:20:30 -0800

    Question Answers

--- client/src/components/graph.js
+++ client/src/components/graph.js
@@ -6,33 +6,73 @@ import {Bar, Line} from 'react-chartjs-2';
 
 const Graph = props => {
 
-    // const [countData, setCountData] = useState({})
+    const [countData, setCountData] = useState({})
+
+    useEffect(  () => {
+
+        setCountData({
+            data: {
+                labels: [props.chartData.labels],
+                datasets: [
+                    {
+                        label: props.chartData.label,
+                        backgroundColor: "rgba(225, 0, 225, 0.75)",
+                        data: props.chartData.map(el => [el.datasets.data])
+                    },
+                    
+                ]
+            },
+        });
+        
+        
+        }, [])
+
 
     console.log(props.chartData)
 
 
         return(
-            <div className="graph-styling" style={{ position: "relative", width: 450, height: 450}}>
-                {props.chartData.map((n, index) => {
-                    return <Bar 
-                                key={index} 
-                                data={n} 
-                                options={{
-                                    responsive: true,
-                                    scales: {
-                                        yAxes: [{
-                                            ticks: {
-                                              beginAtZero: true,
-                                              min: 0,
-                                              max: 100,
-                                              stepSize: 1
-                                          }
-                                      }]
-                                  }
+            // <div className="graph-styling" style={{ position: "relative", width: 450, height: 450}}>
+            //     {props.chartData.map((n, index) => {
+            //         return <Bar 
+            //                     key={index} 
+            //                     data={n} 
+            //                     options={{
+            //                         responsive: true,
+            //                         scales: {
+            //                             yAxes: [{
+            //                                 ticks: {
+            //                                   beginAtZero: true,
+            //                                   min: 0,
+            //                                   max: 100,
+            //                                   stepSize: 1
+            //                               }
+            //                           }]
+            //                       }
             
-                                }}
-                            />;
-                })}
+            //                     }}
+            //                 />;
+            //     })}
+            // </div>
+            <div className="graph-styling" style={{ position: "relative", width: 450, height: 450}}>
+                <h3 style={{ textAlign: "center"}}>ScoreBoard</h3>
+                <Line
+                    options={{
+                        responsive: true,
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                  beginAtZero: true,
+                                  min: 0,
+                                  max: 100,
+                                  stepSize: 1
+                              }
+                          }]
+                      }
+
+                    }}
+                    data={countData.data}
+                    />
             </div>
         )
 }
